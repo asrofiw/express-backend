658cc33e9f0cb1ee76f57dfaef916083db62ddc5
asrofhy <asrofiw@users.noreply.github.com>
2020-09-09 09:35:44 +0700

    Making a HTTP Method POST for console.log using curl library

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
 const app = express()
 
 // GET Path '/home' yang menampilkan respon "Welcome to my first backend!"
@@ -7,5 +7,15 @@ app.get('/home', (req, res) => {
     res.send('Welcome to my first backend!')
 })
 
+// POST Path '/data' yang akan menampilkan data yang dikirim ke "console.log"
+app.use(bodyParser.urlencoded({ exetended: true}))
+app.use(bodyParser.json())
+app.post('/data', (req, res) => {
+    console.log('Got body: ', req.body)
+    res.sendStatus(200)
+})
 
-app.listen(8080)
+
+app.listen(8080, () => {
+    console.log(`Listening Port 8080`)
+})
